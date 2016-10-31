98699d9343b2eef0f4ff9b8cdc6fcbbd38b5e4c1
nguyenchihien <hiennc@onlikeservices.com>
2016-10-31 23:13:14 +0700

    1113

---
+++ assets/setting.js
@@ -0,0 +1,72 @@
+// ============= HOME ==============
+var Page = {
+    //-- de trong "" de giu nguyen default text
+    logoText: "Lê Văn Cát Tường ",
+    subHeading: "Website để thể hiện. <strong>Landing page</strong> để thu hút khách hàng",
+    mainHeading: "Bảo hiểm nhân thọ cho mọi người",
+    CTA: "Tôi muốn có ngay Landing Page",
+    brief: "Cơ hội nhận discount or Free Landing Page. Chỉ cần theo dõi các chương trình cộng đồng của chúng tôi",
+
+    // ============= VARS ==============
+    url : "http://levancattuong.chinhlatoi.top/", //-- co slash cuoi /
+    mainkeyWord : "Le Van Cat Tuong",
+    longkeyWords : ["BHNT", "bảo hiểm nhân thọ, manulife"],
+    facebookAppId : "303370230021409", //-- your appID
+    googleAnalyticId : "UA-82197343-2", //-- your id
+    formId: "https://docs.google.com/forms/d/e/1FAIpQLScjBXL-M0eRB0qhX1xSV5sWWAwsXbNqtjAGm3aq0-l5EZAisg/viewform?embedded=true",
+}
+
+var Social = {
+    //-- de trong "" de giu nguyen default text
+    email: "maito:tuong_f1648@manulife.com.vn",
+    facebook: "https://www.facebook.com/tui.khongbiet.79",
+    twitter: "twitter.com",
+    google: "google.com",
+    instagram: "instagram.com",
+    pinterest: "pinterest.com",
+    linkedin: "linkedin.com"
+}
+
+var colors = [
+    "red","blue","green","orange",  // 0 1 2 3  
+    "pink", "purple", "light-blue", "cyan", // 4 5 6 7  
+    "deep-purple", "indigo", "teal", "lime", // 8 9 10 11
+    "light-green", "yellow", "amber", "deep-orange", // 12 13 14 15
+    "brown", "grey", "blue-grey"      // 16 17 18 19
+];
+
+colors = ['light-green']
+var i = Math.floor( Math.random() * (colors.length - 0) + 0 );
+var chooseColor = colors[i].toString();
+
+
+// ============= PERSONAL ==============
+var Person = {
+    //-- de trong "" de giu nguyen default text
+    logoText: "Lê Văn Cát Tường",
+    fullname: "<strong>Lê Văn Cát Tường</strong>.",
+    slogan: "Insurance Advisor.",
+    sex: '<i class="fa fa-male"></i>', //-- male, female
+    phone: 'tel:0938800307',
+    // message: "Tôi đã sẵn sàng, còn bạn thì sao?", //-- thong diep chinh hien nay!
+    message: "",
+    
+    email: "mailto:tuong_f1648@manulife.com.vn",
+    facebook: 'https://facebook.com/',
+    facebook: "https://www.facebook.com/tui.khongbiet.79",
+    twitter: "twitter.com",
+    google: "google.com",
+    instagram: "instagram.com",
+    pinterest: "pinterest.com",
+    linkedin: "linkedin.com",
+    CTA: "Tôi muốn có ngay Landing Page",
+}
+
+
+var Social = {
+
+}
+
+
+
+    
